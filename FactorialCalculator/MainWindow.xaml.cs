07e2c9f5aa2bfa117308fff1f2d105bd958ede09
Patrick Ingram <bama1172@live.com>
2020-01-14 22:56:04 -0500

    Add project files.

---
+++ FactorialCalculator/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FactorialCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                double inputNum = double.Parse(InputBox.Text); 
+                double calcNum = inputNum; 
+
+                for (double x = calcNum - 1; x >= 1; x--) 
+                {
+                    calcNum *= x; 
+                }
+                
+                if (inputNum < 15) 
+                {
+                    ResBox.Text = calcNum.ToString("###,###");
+                }
+                else
+                {
+                    ResBox.Text = calcNum.ToString("e3");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please Enter a Valid Number", "Error: Wrong Input");
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
